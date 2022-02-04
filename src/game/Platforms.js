8f5814d253fc0ef15f7beb39783662ffc84a2097
AJ <dimezske@gmail.com>
2022-02-04 11:56:42 +1100

    Add files via upload

---
+++ src/game/Platforms.js
@@ -0,0 +1,80 @@
+import Phaser from 'phaser';
+
+class Platforms extends Phaser.Physics.Arcade.Sprite {
+   
+    constructor(scene, x, y, platformId) {
+      super(scene, x, y);
+      
+      this.platformBody = scene.add.sprite(0, 0, 'platform', './assets/ground_sand_broken.png');
+      this.platformWallBody = scene.add.sprite(0,0, 'platformWall', './assets/ground_sand_broken_wall.png');
+      this.setSize(this.platformBody.width, this.platformBody.height);
+      this.setSize(this.platformWallBody.width, this.platformWallBody.height); // DO THIS
+      this.add(this.platformBody);
+      this.add(this.platformWallBody);
+    }
+    generateFloor(){
+      for (i=0;i< 10;++i)
+      {
+        this.add(this.platformWallBody) => {
+          this.platformPlacement.forEach(scene => {
+              x: 500;
+          });
+        }
+      }
+    }
+  }
+
+/*  
+class PlatformWalls extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, platformWallId){
+      super(scene, x, y);
+
+      this.platformWallBody = scene.add.sprite(0,0, 'platformWall', './assets/ground_sand_broken_wall.png')
+       
+      this.setSize(this.platformWallBody.width, this.platformWallBody.height); // DO THIS
+      this.add(this.platformWallBody);
+  }
+}*/
+    /*
+    this.platforms = this.physics.add.staticGroup()
+
+    // then create 5 platforms from the group
+    for (let i = 0; i < 5; ++i)
+    {
+        const x = Phaser.Math.Between(80, 300)
+        const y = 100 * i
+        const platform = this.platforms.create(x, y, 'platform')
+        platform.scale = 0.5
+        const body = platform.body
+        body.updateFromGameObject()
+    } 
+    //this.horizontalWrap(this.player)
+
+		const bottomPlatform = this.findBottomMostPlatform()
+		if (this.player.y > bottomPlatform.y + 200)
+		{
+			this.scene.start('game-over')
+			this.sound.play('dead')
+		}
+
+    findBottomMostPlatform()
+	{
+		const platforms = this.platforms.getChildren()
+		let bottomPlatform = platforms[0]
+
+		for (let i = 1; i < platforms.length; ++i)
+		{
+			const platform = platforms[i]
+
+			// discard any platforms that are above current
+			if (platform.y < bottomPlatform.y)
+			{
+				continue
+			}
+
+			bottomPlatform = platform
+		}
+
+		return bottomPlatform
+	}
+    */           
